4fdfc2f7bb4cdd4dc3bc6eec090a0fb18be661d1
bjkden <denys_boiko@epam.com>
2016-01-28 15:17:58 +0200

    Added merge reports script

---
+++ jtl_reports_merge/merge.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "timeStamp,elapsed,label,responseCode,responseMessage,threadName,success,bytes,grpThreads,allThreads,Latency" > foo1
+cat Loadosophia_1.jtl | grep -v timeStamp >> foo2
+cat Loadosophia_2.jtl | grep -v timeStamp | sed 's/,[^,]*/,1000/'10 >> foo2
+cat Loadosophia_3.jtl | grep -v timeStamp | sed 's/,[^,]*/,1000/'10 >> foo2
+cat Loadosophia_4.jtl | grep -v timeStamp | sed 's/,[^,]*/,1000/'10 >> foo2
+
+cat foo2 | sort -k1 >> foo1
+mv foo1 Merged_Results.jtl
+gzip -9 Merged_Results.jtl
+curl -v https://loadosophia.org/api/file/upload/ -F "token=`cat loadosophia_token.txt`" -F "projectKey=HBR_Perftest" -F "jtl_file=@Merged_Results.jtl.gz"
+rm foo2
